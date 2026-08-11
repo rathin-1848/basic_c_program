9a311a4c0aa3dcd892b3f464363d366eea67fd5b
Rathin Baidya <rathin.baidya2026@gmail.com>
2026-08-11 12:53:11 +0530

    Add Celsius to Fahrenheit conversion program

---
+++ celcious_to_fahrenheit.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+    float c,f;
+    
+    printf("Enter the celcious value: ");
+    scanf("%f", &c);
+    
+    f= ((9*c)/5)+32;
+    
+    printf("The fahrenheit value of %.2f degree celcious: %.2f", c,f);
+    return 0;
+    
+}
